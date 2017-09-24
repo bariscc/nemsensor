a05d30193d096936991bec02ccd39ae4b606811f
Baris Ceylan <bcanceylan@gmail.com>
2017-09-24 18:48:21 +0300

    convert to analog

--- sketch_sep20a/sketch_sep20a.ino
+++ sketch_sep20a/sketch_sep20a.ino
@@ -1,19 +1,37 @@
+int analogPin = A0;   // nemsensor analog output
+float val = 0;        // variable to store the value read
+int setValue = 15;    // TARGET RATE
+
 void setup()
 {
-  pinMode(7, OUTPUT); /* 7 numaralı pini çıkış yaptık */
-  pinMode(8, INPUT);
-  Serial.begin(9600);
+  pinMode(7, OUTPUT); // relay digital output
+  
+  Serial.begin(9600); //debug
 }
 
 void loop()
 {
-  int sensor = digitalRead(8);
-  if(sensor) {
-    digitalWrite(7,HIGH);
-    Serial.print("toprak kuru, vana açılıyor.\n");
-  } else {
+//  int sensor = digitalRead(8);
+//  if(sensor) {
+//    digitalWrite(7,HIGH);
+//    Serial.print("toprak kuru, vana açılıyor.\n");
+//  } else {
+//    digitalWrite(7,LOW);
+//    Serial.print("toprak nemli, vana kapatılıyor.\n");
+//  }
+  val = analogRead(analogPin);
+  float rate = 100 - (val / 1023 * 100);
+  Serial.print("toprak nemi: %");
+  Serial.println((int)rate);
+  Serial.print("hedef nem: %");
+  Serial.println((int)setValue);
+  if(rate > setValue) {
     digitalWrite(7,LOW);
-    Serial.print("toprak nemli, vana kapatılıyor.\n");
+    Serial.println("Sulama Kapalı");
+  } else {
+    digitalWrite(7,HIGH);   
+    Serial.println("Sulama Açık"); 
   }
-  delay(2000);
+  Serial.println("");  
+  delay(5000);
 } 
