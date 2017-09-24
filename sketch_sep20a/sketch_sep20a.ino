6442f31f95804e20efa2d2b636c7737077f30dcd
Baris Ceylan <bcanceylan@gmail.com>
2017-09-24 18:52:14 +0300

    cleanup

--- sketch_sep20a/sketch_sep20a.ino
+++ sketch_sep20a/sketch_sep20a.ino
@@ -1,37 +1,30 @@
 int analogPin = A0;   // nemsensor analog output
-float val = 0;        // variable to store the value read
-int setValue = 15;    // TARGET RATE
+int val = 0;        // variable to store the value read
+int setVal = 15;      // TARGET RATE
 
 void setup()
 {
-  pinMode(7, OUTPUT); // relay digital output
-  
-  Serial.begin(9600); //debug
+  pinMode(7, OUTPUT); // relay digital output  
+  Serial.begin(9600); // debug
 }
 
 void loop()
 {
-//  int sensor = digitalRead(8);
-//  if(sensor) {
-//    digitalWrite(7,HIGH);
-//    Serial.print("toprak kuru, vana açılıyor.\n");
-//  } else {
-//    digitalWrite(7,LOW);
-//    Serial.print("toprak nemli, vana kapatılıyor.\n");
-//  }
   val = analogRead(analogPin);
-  float rate = 100 - (val / 1023 * 100);
+  float rate = 100 - ((float)val / 1023 * 100);
+  
   Serial.print("toprak nemi: %");
   Serial.println((int)rate);
   Serial.print("hedef nem: %");
-  Serial.println((int)setValue);
-  if(rate > setValue) {
+  Serial.println(setVal);
+  
+  if(rate > setVal) {
     digitalWrite(7,LOW);
     Serial.println("Sulama Kapalı");
   } else {
     digitalWrite(7,HIGH);   
     Serial.println("Sulama Açık"); 
   }
-  Serial.println("");  
+  Serial.println("");
   delay(5000);
 } 
